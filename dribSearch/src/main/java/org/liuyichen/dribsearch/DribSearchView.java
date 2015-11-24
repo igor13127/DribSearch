913f3a53d16fe096a6cc93fea4cedeb75aed13be
igor13127 <igor.eschalier@gmail.com>
2015-11-24 11:31:52 +0100

    add getmState of dribsearch

--- dribSearch/src/main/java/org/liuyichen/dribsearch/DribSearchView.java
+++ dribSearch/src/main/java/org/liuyichen/dribsearch/DribSearchView.java
@@ -493,4 +493,8 @@ public class DribSearchView extends View implements ValueAnimator.AnimatorUpdate
             }
         };
     }
+
+    public State getmState(){
+        return mState;
+    }
 }
